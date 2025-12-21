ac6f3e54150de961d8517ba53d45e783f3a7c143
hawaiianborgir <shinkyu49@gmail.com>
2025-12-21 16:37:59 +0900

    Add operate function

--- script.js
+++ script.js
@@ -14,3 +14,15 @@ function divide(a, b) {
     return a / b;
 }
 
+function operate(operator, a, b) {
+    switch(operator) {
+        case adding:
+            add(a, b);
+        case subtracting:
+            subtract(a, b);
+        case multiplying:
+            multiply(a, b);
+        case dividing:
+            divide(a, b);
+    }
+}
